ae302d32bb5558727f2fac60bd9e4ba16775bc12
peritissimus <iamkushal.iitkgp@gmail.com>
2022-01-13 12:47:52 +0530

    logo updates

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -50,7 +50,9 @@ const Banner = () => {
           </Flex>
         </div>
         <Flex as="figure" sx={styles.illustration}>
-        <Box sx={{ margin:'50px' }}><iframe width="560" height="315" src="https://www.youtube.com/embed/6ERXfZTFbFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Box>
+        <Box sx={{ margin:'50px' }}>
+          <iframe width="560" height="315"  src="https://www.youtube.com/embed/6ERXfZTFbFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </Box>
 
         </Flex>
       </Container>
